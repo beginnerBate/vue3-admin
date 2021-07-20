35b77f52a55bfb69ee996efb6bc76d9fc06298eb
liuyeqian <liuyeq2015@163.com>
2021-07-20 16:56:10 +0800

    '构建项目,配置全局样式，路由，elementUI'

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    redirect:"login"
+  },
+  {
+    path: "/login",
+    name: "Login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../views/Login"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
---
+++ src/views/Login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">login</div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+.login{
+  height: 100vh;
+  background-color: aquamarine;
+}
+</style>
